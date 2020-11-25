1e264e12dd5507944ee3bc0e606610d47500e629
kaygb <kaygbtop@163.com>
2020-11-25 15:59:01 +0800

    Initial commit

---
+++ app/src/main/java/com/kaygb/fly_game/view/SenserMan.java
@@ -0,0 +1,31 @@
+//package com.nejidev.android_fly_game.view;
+//
+//import android.content.Context;
+//import android.hardware.Sensor;
+//import android.hardware.SensorEvent;
+//import android.hardware.SensorEventListener;
+//import android.hardware.SensorManager;
+//
+//import static androidx.core.content.ContextCompat.getSystemService;
+//
+//public class SenserMan implements SensorEventListener {
+//
+//    public SensorManager mSensorManager;
+//
+//    public SenserMan(SensorManager mSensorManager) {
+//        this.mSensorManager = mSensorManager;
+//        // 获取传感器管理对象
+//        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//
+//    }
+//
+//    @Override
+//    public void onSensorChanged(SensorEvent event) {
+//
+//    }
+//
+//    @Override
+//    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//
+//    }
+//}
